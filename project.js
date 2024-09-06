f1569100274709dceb2b122617ead1f4e9793cd1
RinRin <merincyriac2k@gmail.com>
2024-09-06 23:49:36 +0530

    calculator code

--- project.js
+++ project.js
@@ -1,2 +1,17 @@
-console.log('hellooooooooo world');
-console.log('this is the change im making in github');
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
